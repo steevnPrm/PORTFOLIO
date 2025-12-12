13f904064704aec466a6bc887e009487223ce4a7
bayesOnTheWeb <steevn.pyram@gmail.com>
2025-12-12 17:42:31 +0100

    delte prisma 2

--- nextjs-prisma/prisma/migrations/20251202212402_add_contact/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Contact" (
-    "id" SERIAL NOT NULL,
-    "author" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-
-    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
-);
